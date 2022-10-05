2578d04f9b2fd96d9eb5d307ea2bd83a367d7947
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2022-10-06 00:11:53 +0530

    start js works

---
+++ script.js
@@ -0,0 +1,14 @@
+let noteTitle = document.getElementById("note_title");
+let noteContent = document.getElementById("note_content");
+let addNote = document.getElementById("add_note");
+let titleLength;
+let contentLength;
+
+addNote.addEventListener("click", function(e){
+    titleLength = noteTitle.value.length;
+    contentLength = noteContent.value.length;
+    
+    
+
+
+})
