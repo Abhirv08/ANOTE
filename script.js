d63d01290ac4ef2a97195fc72074a456f70c2dbf
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2022-10-31 00:14:51 +0530

    add delete and edit functionalities to the notes

--- script.js
+++ script.js
@@ -2,67 +2,101 @@ let noteTitleBox = document.getElementById("note_title");
 let noteContentBox = document.getElementById("note_content");
 let noteTitle, noteContent;
 let addNote = document.getElementById("add_note");
+let updateNote = document.getElementById("update_note");
 let container = document.getElementById("Container");
 let i = localStorage.length;
 
-window.addEventListener("DOMContentLoaded", function () {
-    for (var j = 0; j < localStorage.length; j++) {
-        var item = JSON.parse(localStorage.getItem(j))
-        addToScreen(item.title, item.desc, j);
-    }
-})
 
-addNote.addEventListener("click", function () {
-    noteTitle = toPascalCase(noteTitleBox.value);
-    noteContent = noteContentBox.value;
-
-    let titleLength = noteTitle.length;
-    let contentLength = noteContent.length;
-
-    if (titleLength === 0 || contentLength === 0) {
-        window.alert("Please fill all details");
-    } else {
-        addToLocalStorage(noteTitle, noteContent);
-    }
-    noteTitleBox.value = "";
-    noteContentBox.value = "";
-})
+window.addEventListener("DOMContentLoaded", displayNotes())
 
 function toPascalCase(s) {
     return s.slice(0, 1).toUpperCase() + s.slice(1).toLowerCase();
 }
 
-function addToLocalStorage(noteTitle, noteContent) {
-    let note = {
-        title: noteTitle,
-        desc: noteContent
+function getNotes() {
+    const notes = JSON.parse(localStorage.getItem("ANOTE-notes")) || [];
+
+    // return notes.sort((a, b) => {
+    //     return new DataTransfer(b.updated) > new Date(a.updated) ? 1 : -1;
+    // });
+    return notes;
+}
+
+
+
+// Add new Note to array as well as local Storage
+addNote.addEventListener("click", () => {
+    const notes = getNotes();
+    let title = toPascalCase(noteTitleBox.value);
+    let desc = noteContentBox.value;
+    const note = { title: title, desc: desc }
+    if (title.length > 0 && desc.length > 0) {
+        notes.push(note);
+        localStorage.setItem("ANOTE-notes", JSON.stringify(notes));
+    } else {
+        window.alert("Please fill title and description");
     }
-    addToScreen(noteTitle, noteContent, i);
-    localStorage.setItem(i, JSON.stringify(note));
-    i++;
-}
+    displayNotes();
+    noteTitleBox.value = "";
+    noteContentBox.value = "";
+})
 
-function addToScreen(noteTitle, noteContent, i) {
-    console.log(i)
-    container.innerHTML +=
-        `<div class = "notes">
-            <div class="nav_function">
-                <p id="listedNoteTitle">${noteTitle}</p>
-                <div class="functions">
-                    <div id="${i}" onclick="editNote(this.id)"><img src="./images/edit.png" /></div>
-                    <div id="${i}" onclick = "deleteNote(this.id)"><img src="./images/delete.png" /></div>
+// Display all notes available to the user
+function displayNotes() {
+    const notes = getNotes();
+    let allNotes = "";
+
+    for (let i = 0; i < notes.length; i++) {
+        allNotes +=
+            `<div class = "notes">
+                <div class="nav_function">
+                    <p id="listedNoteTitle">${notes[i].title}</p>
+                    <div class="functions">
+                        <div id="${i}" onclick="editNote(this.id)"><img src="./images/edit.png" /></div>
+                        <div id="${i}" onclick = "deleteNote(this.id)"><img src="./images/delete.png" /></div>
+                    </div>
                 </div>
-            </div>
-            <div id="listedNoteContent">${noteContent}</div>
-        </div>`
-}   
-
-
-function deleteNote(id){
-    localStorage.removeItem(id);
-    updateLocalStorage();
+                <div id="listedNoteContent">${notes[i].desc}</div>
+            </div>`
+    }
+    container.innerHTML = allNotes;
 }
 
-function editNote(id){
-    console.log(id)
+function deleteNote(idx) {
+    const notes = getNotes();    
+
+    notes.splice(idx, 1);
+    localStorage.setItem("ANOTE-notes", JSON.stringify(notes));
+    displayNotes();
 }
+
+function editNote(idx) {
+    addNote.classList.add("hide_button")
+    updateNote.classList.remove("hide_button");
+    const notes = getNotes();
+
+    noteTitleBox.value = notes[idx].title;
+    noteContentBox.value = notes[idx].desc;
+    notes.splice(idx, 1);
+    localStorage.setItem("ANOTE-notes", JSON.stringify(notes));
+    displayNotes();
+}
+
+updateNote.addEventListener("click", () => {
+    addNote.classList.remove("hide_button")
+    updateNote.classList.add("hide_button");
+
+    const notes = getNotes();
+    let title = toPascalCase(noteTitleBox.value);
+    let desc = noteContentBox.value;
+    const note = { title: title, desc: desc }
+    if (title.length > 0 && desc.length > 0) {
+        notes.push(note);
+        localStorage.setItem("ANOTE-notes", JSON.stringify(notes));
+    } else {
+        window.alert("Please fill title and description");
+    }
+    displayNotes();
+    noteTitleBox.value = "";
+    noteContentBox.value = "";
+})  
